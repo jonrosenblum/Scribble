62fbda6adbbf887333ce87176ef656684eb36877
Jon Rosenblum <jon.m.rosenblum@gmail.com>
2023-10-22 11:28:08 -0400

    Task Service for connecting frontend to API

---
+++ frontend/src/app/task.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  constructor() { }
+
+  createList(title: string){
+    // We want to send a web request to create a list 
+  }
+}

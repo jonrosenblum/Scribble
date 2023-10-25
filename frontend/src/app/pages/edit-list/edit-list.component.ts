3473c8246effe2880f6564b5f5e614ba7e5e7620
Jon Rosenblum <jon.m.rosenblum@gmail.com>
2023-10-25 18:22:08 -0400

    Edit Specified List for Authenticated Suer

---
+++ frontend/src/app/pages/edit-list/edit-list.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { TaskService } from 'src/app/task.service';
+
+@Component({
+  selector: 'app-edit-list',
+  templateUrl: './edit-list.component.html',
+  styleUrls: ['./edit-list.component.scss'],
+})
+export class EditListComponent {
+  constructor(
+    private route: ActivatedRoute,
+    private taskService: TaskService,
+    private router: Router
+  ) {}
+
+  listId: string;
+
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      this.listId = params.listId;
+      console.log(params.listId);
+    });
+  }
+
+  updateList(title: string) {
+    this.taskService.updateList(this.listId, title).subscribe(() => {
+      this.router.navigate(['/lists', this.listId]);
+    });
+  }
+}

cf68f4a662f3f3b1a091277ff7572159792b0fd1
Jon Rosenblum <jon.m.rosenblum@gmail.com>
2023-10-25 18:21:32 -0400

    Sign Up Page

---
+++ frontend/src/app/pages/sign-up/sign-up.component.ts
@@ -0,0 +1,24 @@
+import { HttpResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth.service';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.scss'],
+})
+export class SignUpComponent implements OnInit {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit() {}
+
+  onSignupButtonClicked(email: string, password: string) {
+    this.authService
+      .signup(email, password)
+      .subscribe((res: HttpResponse<any>) => {
+        console.log(res);
+        this.router.navigate(['/lists']);
+      });
+  }
+}

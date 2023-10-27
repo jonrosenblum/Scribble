854809fdf25188f9c6358d416ba8c27ea4b81e6f
linuxdev <asieduernest@yahoo.co.uk>
2023-10-27 00:43:28 -0400

    fix: serving angular in docker with host 0.0.0.0

--- api/app.js
+++ api/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 const { mongoose } = require('./db/mongoose');
+const path = require('path');
 
 const bodyParser = require('body-parser');
 
